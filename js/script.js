9d6d7a91a93637bb42748796a33d1f5961b885ff
Evgenik <109359580+Jeko26@users.noreply.github.com>
2022-08-13 23:57:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
